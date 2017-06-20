7ceb7865ab6cb671f600d3d247a560e19ec8330a
Bhami <jainbhami@gmail.com>
2017-06-20 12:08:38 -0400

    Bhami created program arrayTest

---
+++ coreJavaPractice/src/com/bhami/coreJava/ArraySum.java
@@ -0,0 +1,30 @@
+package com.bhami.coreJava;
+
+public class ArraySum {
+
+	public static void main(String[] args) {
+		System.out.println("Doing array sum");
+		StringBuilder sb = new StringBuilder();
+		int out = 11;
+		int iteration = 0;
+		int found = 0;
+		int[] inputArray = {2, 3, 6, 4, 8, 1, 0, 9, 2};
+		int len = inputArray.length;
+		for(int i=0; i<len; i++){
+			for(int j= i+1; j<len; j++){
+				int sum = inputArray[i] + inputArray[j];
+				if (sum == out){
+					System.out.println("Found a pair (" + inputArray[i] + "," + inputArray[j] +")" );
+					found++;
+				}
+			}
+			iteration++;
+		}
+		System.out.println("no of iteration : " +iteration);
+		if (found != 0)
+			System.out.println("Found :" + found + " pairs");
+		else
+			System.out.println("Found :" + found + " pairs");
+		
+	}
+}
