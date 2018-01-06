e05d389f51ef5202aeab229b3c984f8ffcc772f2
Bhami <jainbhami@gmail.com>
2018-01-05 19:59:30 -0500

    Testing ATM application

--- coreJavaPractice/src/com/bhami/coreJava/singleton/ATM/MiniStatement.java
+++
@@ -1,9 +0,0 @@
-package com.bhami.coreJava.singleton.ATM;
-
-public class MiniStatement implements StatementType {
-    @Override
-    public String print() {
-        System.out.println("Mini Statement Created");
-        return "miniStmt";
-    }
-}
